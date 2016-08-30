c3109ebc513fb807aa4208f449be11a1e7993bb4
프로그래밍7\USER <nhy3795@naver.com>
2016-08-30 20:17:31 +0900

    업데이트 다시 로드함 //main에서 input으로 이름 바꿈

--- Project/Update.cpp
+++ Project/Update.cpp
@@ -1,3 +1,45 @@
 #include "Common.h"
 
 int MAXTARGET; // 맵에 있는 타겟의 수를 저장함.
+#include "Common.h"
+
+void input()
+{
+	char ch;
+	int x, y;
+
+	for (; 2;)
+	{
+		ch = getch();
+
+		if (ch == 0xE0 || ch == 0)
+		{
+			switch (ch)
+			{
+			case LEFT:
+			case RIGHT:
+			case UP:
+			case DOWN:
+				Move(ch);
+				break;
+			}
+		}
+		else
+		{
+			ch = tolower(ch);
+
+			if (ch == 'r')
+				break;
+			if (ch == 'q')
+				exit(0);
+
+		}
+	}
+}
+
+void Move(char ch)
+{
+	int dx = 0, dy = 0;
+
+
+}
