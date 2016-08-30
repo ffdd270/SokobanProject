a3a984e40d6ddf561f77b6e9d21f076c2d910ec9
프로그래밍7\USER <nhy3795@naver.com>
2016-08-30 19:23:26 +0900

    MapToll

--- Project/Common.h
+++ Project/Common.h
@@ -5,5 +5,10 @@
 #include <stdio.h>
 #include <conio.h>
 
+enum Sokoban
+{
+	SPACE, PLAYER, TARGET, DELIVERY, //공백, 플레이어, 목표지점, 배달물
+	PLAYER_ON_TARGET, DELIVERY_ON_TARGET,  //타겟위에 플레이어, 타겟위에 배달물
+};
 
 #endif
