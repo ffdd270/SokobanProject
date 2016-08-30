36478dc9580db6b7df0f053b432068bc2c8b0e29
프로그래밍12\USER <allice1010>
2016-08-30 20:05:24 +0900

    no message

--- Project/Update.cpp
+++ Project/Update.cpp
@@ -1,1 +1,42 @@
 #include "Common.h"
+
+void main()
+{
+	char ch;
+	int x, y;
+
+	for (; 2;)
+	{
+		ch = getch();
+
+		if (ch == 0xE0 || ch == 0)
+		{
+			switch (ch)
+			{
+			case LEFT:
+			case RIGHT:
+			case UP:
+			case DOWN:
+				Move(ch);
+				break;
+			}
+		}
+		else
+		{
+			ch = tolower(ch);
+
+			if (ch == 'r')
+				break;
+			if (ch == 'q')
+				exit(0);
+
+		}
+	}
+}
+
+void Move(char ch)
+{
+	int dx = 0, dy = 0;
+
+
+}
