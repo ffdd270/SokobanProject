1f564d5e7c01c5909b5c6cb7856debe9ba517547
프로그래밍7\USER <nhy3795@naver.com>
2016-09-01 21:22:29 +0900

    32

--- Project/Sound.cpp
+++ Project/Sound.cpp
@@ -67,52 +67,76 @@ void Release()
 	FMOD_System_Release(g_System);
 }
 
-int main(void)
+void Play(int SongNum)
+{
+	static int OldMusic = -1; //전음악, -1이면 아직 전음악이 없었던 상황
+	int MyMusic = SongNum;
+
+	if ((OldMusic != -1) && (OldMusic != MyMusic))
+	{
+		FMOD_Sound_Release(g_arr_Sound[OldMusic]);
+	}
+	if (OldMusic != MyMusic)
+	{
+		FMOD_System_CreateSound(g_System, g_arr_songname[SongNum], FMOD_LOOP_NORMAL, NULL, &g_arr_Sound[SongNum]);
+	}
+	OldMusic = MyMusic;
+}
+
+int main()
 {
 	int nKey;
-	FMOD_CHANNEL *channel = NULL;
+	FMOD_CHANNEL * channel = NULL;
 	float volume = 0.5f;
 	FMOD_BOOL IsPlaying;
-
+	int SongNum = 0;
 	Init();
 
 	while (1)
 	{
-		if (_kbhit())  // 키 입력 감지   
+
+		if (_kbhit())
 		{
-			nKey = _getch();   // 입력된 키 값을 키보드 버퍼로부터 가져옴
-			if (nKey == 'q')  // q 키가 눌리면 프로그램 종료, 키 입력중 가장 최우선 순위가 된다. 
+			nKey = getch();
+			if (nKey == 'q')
 				break;
 
 			switch (nKey)
 			{
+			case 'e':
+				FMOD_Channel_Stop(channel);
+				SongNum++;
+				break;
+			case 'd':
+				FMOD_Channel_Stop(channel);
+				SongNum--;
+				break;
 			case 'p':
-				FMOD_System_PlaySound(g_System, FMOD_CHANNEL_FREE, g_Sound, 0, &channel);
+				Play(SongNum);
+				FMOD_System_PlaySound(g_System, FMOD_CHANNEL_FREE, g_arr_Sound[SongNum], 0, &channel);
 				break;
 			case 's':
 				FMOD_Channel_Stop(channel);
 				break;
-			case 'u': // 볼륨 업
+			case 'x':
+				if (volume > -0.01f)
+					volume = 0.0f;
+				volume -= 0.1f;
+				FMOD_Channel_SetVolume(channel, volume);
+				break;
+			case 'u':
 				volume += 0.1f;
 				if (volume > 1.0f)
 					volume = 1.0f;
 				FMOD_Channel_SetVolume(channel, volume);
 				break;
-			case 'd': // 볼륨 다운
-				volume -= 0.1f;
-				if (volume < 0.0f)
-					volume = 0.0f;
-				FMOD_Channel_SetVolume(channel, volume);
-				break;
 			}
+			printf("Number : %d ", SongNum);
 		}
-
-		FMOD_Channel_IsPlaying(channel, &IsPlaying); // 사운드 출력중인지 체크
+		FMOD_Channel_IsPlaying(channel, &IsPlaying);
 		if (IsPlaying == 1)
-			FMOD_System_Update(g_System);		 	   // FMOD 업데이트	 
+			FMOD_System_Update(g_System);
 	}
-
 	Release();
 	return 0;
 }
-
