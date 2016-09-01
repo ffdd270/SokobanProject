8d5a85997a2f1028423483d969b0595c058aa986
프로그래밍7\USER <nhy3795@naver.com>
2016-09-01 21:04:24 +0900

    맵툴 수정 사운드 파일 수정중

--- Project/Sound.cpp
+++ Project/Sound.cpp
@@ -1,23 +1,68 @@
-
-#include <stdio.h>
-#include <conio.h>
+#include "Common.h"
 #include <fmod.h>
+#include <io.h>
 
 FMOD_SYSTEM *g_System;  // Note: FMOD system 포인터 변수 선언
-FMOD_SOUND  *g_Sound;   // Note: FMOD Sound 포인터 변수 선언
+FMOD_SOUND  **g_arr_Sound;   // Note: FMOD Sound 포인터 변수 선언
+
+
+char ** g_arr_songname;
+
+int h_Fsys = 0;
+int h_Fsnd = 0;
+int g_SongNumber = 0;
 
 void Init()
 {
 	// Note: FMOD 사운드 초기화 및 사운드 로딩
+	_finddata_t fd;
+	long handle;
+	int result = 1;
+	handle = _findfirst(".\\*.mp3", &fd);
+
+	if (handle == 1)
+	{
+		system("cls");
+		printf("system Failed : No Mp3 File in Here. ");
+		_getch();
+		exit(1);
+	}
+
+	//곡의 갯수 확인.
+	while (result != -1)
+	{
+		result = _findnext(handle, &fd);
+		g_SongNumber++;
+	}
+
+	g_arr_songname = (char **)malloc(sizeof(char *) * g_SongNumber);
+	g_arr_Sound = (FMOD_SOUND **)malloc(sizeof(FMOD_SOUND *)* g_SongNumber);
+
+	handle = _findfirst(".\\*,mp3", &fd);
+	int i = 0;
+	result = 0;
+
 	FMOD_System_Create(&g_System);
 	FMOD_System_Init(g_System, 32, FMOD_INIT_NORMAL, NULL);
-	FMOD_System_CreateSound(g_System, "run.wav", FMOD_DEFAULT, 0, &g_Sound);
+
+	while (result != -1)
+	{
+		g_arr_songname[i] = (char *)malloc(strlen(fd.name));
+		strcpy(g_arr_songname[i], fd.name);
+		result = _findnext(handle, &fd);
+		i++;
+	}
 }
 
 void Release()
 {
 	// Note: FMOD 해제
-	FMOD_Sound_Release(g_Sound);
+	for (int i = 0; i < g_SongNumber; i++)
+	{
+		FMOD_Sound_Release(g_arr_Sound[i]);
+		free(g_arr_Sound[i]);
+	}
+
 	FMOD_System_Close(g_System);
 	FMOD_System_Release(g_System);
 }
